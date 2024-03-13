fdd31c869beae9bc93a74f90ac2e13dbee49c1bb
mohd aman <mohdastar200026@gmail.com>
2024-03-13 16:24:16 +0530

    base functionlity is complete, database integration remains

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,14 +1,34 @@
 import { NextResponse } from "next/server";
+import { PDFDocument } from "pdf-lib";
 
 export const POST = async (req) => {
 
     const data = await req.formData();
-    const file = data.get('file');
-    console.log(file);
 
-    return NextResponse.json({
-        success: true
+    const file = await data.get('file');
+
+    const pdfDoc = await PDFDocument.create();
+
+    const imgBuffer = await file.arrayBuffer();
+
+    const pngImage = await pdfDoc.embedJpg(imgBuffer);
+
+    const page = pdfDoc.addPage();
+
+    page.drawImage(pngImage, {
+        x: 0,
+        y: 0,
+        width: page.getWidth(),
+        height: page.getHeight()
+    });
+    const pdfBytes = await pdfDoc.save();
+
+    const res = new NextResponse(pdfBytes, {
+        headers: {
+            "Content-Type": "application/pdf",
+        },
+        status: 200,
     })
-
+    return res
 }
 
--- src/components/Capture.js
+++ src/components/Capture.js
@@ -1,33 +1,60 @@
 'use client'
 
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 
 const Capture = () => {
 
     const [image, setImage] = useState(null);
+    const [urls, setUrls] = useState([]);
 
-      const handleSubmit = async (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         const data = new FormData();
+        if(!image)return;
         data.set('file', image);
 
         const res = await fetch(`/api/upload`, {
-          method: 'POST',
-          body: data,
+            method: 'POST',
+            body: data
         });
 
-        console.log(res);
-      }
+        const blob = await res.blob();
+    
+        const pdfBlob = new Blob([blob], { type: 'application/pdf' });
+
+        setUrls((prevState) => [...prevState, URL.createObjectURL(pdfBlob)]);
+
+    }
 
     return (
         <section>
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <input type='file' name='image' accept='image/*' capture onChange={e => setImage(e.target.files[0])} />
-                    <button type='submit'>submit</button>
+            <div className='flex flex-col items-center justify-center'>
+                <h1 className='m-1 text-4xl text-gray-400 font-bold text-decoration-line: underline'>Select a Image</h1>
+                <form onSubmit={handleSubmit} className='flex flex-col items-center justify-center border-4 p-10 w-96 m-5 rounded-lg'>
+                    <input
+                        type='file'
+                        name='image'
+                        accept='image/*'
+                        capture
+                        onChange={e => setImage(e.target.files[0])}
+                        className='border-4 rounded-lg p-2'
+                    />
+                    <button type='submit' className='border-4 rounded-lg p-2 bg-green-500 text-white mt-8'>
+                        submit
+                    </button>
                 </form>
-                
-                {image && <img src={image} />}
+
+
+                <ul className='my-4 border-4 p-4 w-96 m-5 rounded-lg'>
+                    {urls.map((url,index) => (
+                        <li key={index} className='flex items-center justify-center my-2'>
+                            <span>{index+1}</span>
+                            <a href={url} download="filename.pdf" className='border-4 rounded-lg p-2 bg-indigo-500 text-white ml-8'>Download PDF</a>
+                        </li>
+                    ))}
+                </ul>
+
+
             </div>
         </section>
     )
